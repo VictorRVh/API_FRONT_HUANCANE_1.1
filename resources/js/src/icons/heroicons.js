084204bbce72ba170cd6c19a68b915a668c634a0
Victor Raul <victorraulvaldez54@gmail.com>
2024-10-31 08:06:09 -0700

    holavit!

--- resources/js/src/icons/heroicons.js
+++ resources/js/src/icons/heroicons.js
@@ -3,25 +3,6 @@ import { defineAsyncComponent } from 'vue';
 
 // Importa los íconos en estilo outline
 const icons = {
-<<<<<<< HEAD
-  HomeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/HomeIcon')),
-  BeakerIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/BeakerIcon')),
-  UserIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/UserIcon')),
-  AcademicCapIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/AcademicCapIcon')), 
-  UsersIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/UsersIcon')),
-  BookOpenIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/BookOpenIcon')),
-  BuildingOfficeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/BuildingOfficeIcon')), 
-  PresentationChartLineIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/PresentationChartLineIcon')), 
-  ChartBarIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/ChartBarIcon')), 
-  FolderIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/FolderIcon')), 
-  BookmarkIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/BookmarkIcon')), 
-  BookmarkSquareIcon: defineAsyncComponent(() => import('@heroicons/vue/24/solid/BookmarkSquareIcon')), 
-  EyeIcon: defineAsyncComponent( () => import('@heroicons/vue/24/solid/EyeIcon')),
-  PencilSquareIcon: defineAsyncComponent( () => import('@heroicons/vue/24/solid/PencilSquareIcon')),
-  TrashIcon: defineAsyncComponent( () => import('@heroicons/vue/24/solid/TrashIcon')),
-  CalendarIcon: defineAsyncComponent( () => import('@heroicons/vue/24/solid/CalendarIcon')),
-  // Agrega más íconos aquí según sea necesario academic-
-=======
   HomeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/HomeIcon')),
   BeakerIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BeakerIcon')),
   UserIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserIcon')),
@@ -37,8 +18,8 @@ const icons = {
   EyeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/EyeIcon')),
   PencilSquareIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PencilSquareIcon')),
   TrashIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/TrashIcon')),
+  CalendarIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/CalendarIcon')),
   // Agrega más íconos aquí según sea necesario
->>>>>>> ac338da583dccfaa4633c8be0af139d445e9e29c
 };
 
 // Registra los íconos como componentes globales
